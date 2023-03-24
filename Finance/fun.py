eab85e02d86f04e6f38e3875fc40d51f39f845e6
Atomic Script <46688597+AtomicScript@users.noreply.github.com>
2023-03-25 01:31:58 +0400

    added RSI & added MA

--- Finance/fun.py
+++ Finance/fun.py
@@ -4,6 +4,13 @@ import os
 import numpy as np
 import requests
 
+MA_PERIOD_1 = 20
+MA_PERIOD_2 = 90
+MA_PERIOD_3 = 120
+
+RSI_PERIOD = 14
+RSI_BUY_THRESHOLD = 30
+RSI_SELL_THRESHOLD = 70
 
 API_KEY =  os.getenv('Binance_API_KEY')
 API_SECRET =  os.getenv('Binance_SECRET_KEY')
@@ -31,5 +38,31 @@ def get_price(coin):
 
     return price
 
-while True:
-    print('Current price $' + str(get_price('BTCUSDT')) + ' Change in 24 hours ' + str(get_price_change_24('BTCUSDT')))
+def get_moving_average_price(coin):
+    prices = get_prices(coin)
+    ma_1 = np.mean(prices[-MA_PERIOD_1:])
+    ma_2 = np.mean(prices[-MA_PERIOD_2:])
+    ma_3 = np.mean(prices)
+    return ma_1, ma_2, ma_3
+
+def get_RSI(coin):
+    prices = get_prices(coin)
+    changes = np.diff(prices)
+    ups = np.clip(changes, 0, np.inf)
+    downs = np.clip(-changes, 0, np.inf)
+    rsi = 100 - (100 / (1 + (np.mean(ups[-RSI_PERIOD:]) / np.mean(downs[-RSI_PERIOD:]))))
+
+    if rsi > RSI_SELL_THRESHOLD:
+        state = 'OverBought Potential Sell'
+    elif rsi < RSI_BUY_THRESHOLD:
+        state = 'OverSold Potential Buy'
+    else:
+        state = 'Hold'
+
+    return rsi, state
+
+
+print('Current price $' + str(get_price('BTCUSDT')) + ' Change in 24 hours ' + str(get_price_change_24('BTCUSDT')))
+print(f'15D Moving average is {round(float(get_moving_average_price("BTCUSDT")[0]),2)}')
+print(f'RSI : {get_RSI("BTCUSDT")[0]}, STATE : {get_RSI("BTCUSDT")[1]}')
+print(f'RSI : {round(float(get_RSI("BTCUSDT")[0]))}, STATE : {get_RSI("BTCUSDT")[1]}')
